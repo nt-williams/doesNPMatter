07fb972aaa87c18c35a264921e26eee1379745ac
Nick Williams <ntwilliams.personal@gmail.com>
2021-06-10 11:35:56 -0400

    simulate script updates for detecting local run.

--- scripts/nsimulate.R
+++ scripts/nsimulate.R
@@ -20,11 +20,6 @@ sim_config <- args[1]
 # capture Slurm task id
 slurm_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
-if (is.na(slurm_id)) {
-  # Slurm id isn't created during tests locally
-  slurm_id <- 1
-}
-
 # capture config file
 task <- get(file = "./scripts/config.yml", config = sim_config)
 
@@ -33,16 +28,31 @@ dgps <- readRDS(task$dgp_path)
 machines <- task$machines
 K <- length(dgps)
 
+if (is.na(slurm_id)) {
+  # Slurm id isn't created during local runs
+  # the number of machines will also be overridden in local runs
+  slurm_id <- 1
+  machines <- 1
+}
+
 # based on the Slurm task, split up the DGMs to be evaluated by the machines
 do <- (1:K)[(0:(K - 1)) %/% (K / machines) + 1 == slurm_id]
 
 # for DGMs to be used, simulate data and estimate ATE using method specified in config
-results <- list()
-for (i in 1:length(do)) {
-  results[[i]] <- try(
-    simulate(dgps[[i]], task$nobs, reps = task$reps, method = task$method)
-  )
+run <- function() {
+  results <- list()
+  pb <- txtProgressBar()
+  for (i in 1:length(do)) {
+    setTxtProgressBar(pb, i / length(do))
+    results[[i]] <- try(
+      simulate(dgps[[i]], task$nobs, reps = task$reps, method = task$method)
+    )
+  }
+  close(pb)
+  results
 }
+
+results <- run()
 
 # save results with a unique identifier for the configuration and Slurm task
 signature <- paste0(task$signature, "_", slurm_id, ".rds")
