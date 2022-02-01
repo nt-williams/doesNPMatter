b515e893c1ccfcbe2e2bb141f1c62440e94800ca
Nick Williams <ntwilliams.personal@gmail.com>
2022-02-01 15:27:41 -0800

    First pass at script for generating dgps on cluster.

--- scripts/generate_dgps.R
+++ scripts/generate_dgps.R
@@ -1,43 +1,44 @@
+library(glue)
+
 source("R/dgp.R")
 
 id <- Sys.getenv("SGE_TASK_ID")
 
 args <- commandArgs(trailingOnly = TRUE)
 
+# FOR TESTING: 
 # args <- list(
 #   n_bin = 3,
 #   n_num = 1,
 #   inter_order = 2,
-#   hte = TRUE,
-#   conf_bias = 0.1,
-#   pos_bound = 50,
-#   npoints = 50,
-#   eta = 4,
-#   rho = 1,
-#   tol = 0.01
+#   hte = TRUE
 # )
 
 n_bin <- as.numeric(args[[1]])
 n_num <- as.numeric(args[[2]])
 inter_order <- as.numeric(args[[3]])
 hte <- as.logical(args[[4]])
-conf_bias <- as.numeric(args[[5]])
-pos_bound <- as.numeric(args[[6]])
-npoints <- as.numeric(args[[7]])
-eta <- as.numeric(args[[8]])
-rho <- as.numeric(args[[9]])
+conf_bias <- runif(1, -0.3, 0.3) # as.numeric(args[[5]])
+pos_bound <- 1000 # as.numeric(args[[6]])
+npoints <- 50 # as.numeric(args[[7]])
+eta <- runif(1, 0.1, 10) # as.numeric(args[[8]])
+rho <- runif(1, 0.1, 10) # as.numeric(args[[9]])
 
 out <- try(dgp(
-  n_bin = n_bin, n_num = n_num, 
-  inter_order = inter_order, 
-  hte = hte, conf_bias = conf_bias,
-  pos_bound = pos_bound, npoints = npoints, 
-  eta = eta, rho = rho, tol = 0.01
+  n_bin = n_bin,
+  n_num = n_num,
+  inter_order = inter_order,
+  hte = hte,
+  conf_bias = conf_bias,
+  pos_bound = pos_bound,
+  npoints = npoints,
+  eta = eta,
+  rho = rho,
+  tol = 0.01
 ))
 
-if (class(out) == "try-error") quit('no')
+if (class(out) == "try-error") quit("no")
 
-saveRDS(
-  out, 
-  glue::glue("data/sims/DGP_{n_bin}_{n_num}_{inter_order}_{hte}_{conf_bias}_{pos_bound}_{npoints}_{eta}_{rho}_{id}.rds")
-)
+saveRDS(out, glue("data/dgps/DGP_{n_bin}_{n_num}_{inter_order}_{hte}/{id}.rds"))
+
+quit("no")
