c2e4956a34d05c6445e090206a24602f977ce1ab
Nick Williams <ntwilliams.personal@gmail.com>
2022-02-01 16:48:38 -0800

    Generate dgms shell script for partition.

---
+++ scripts/generate_dgps.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#$ -l mem=4G,time=:20:
+cd doesNPMatter
+Rscript=/nfs/apps/R/4.0.3/bin/Rscript
+export R_LIBS_USER=/ifs/home/msph/epi/ntw2117/R_4.0
+${Rscript} scripts/generate_dgps.R $1 $2 $3 $4
