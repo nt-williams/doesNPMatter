016411dce2e04b355ac45e1f660e7cd00f0a9e4c
Nick Williams <ntwilliams.personal@gmail.com>
2022-02-02 13:17:57 -0800

    Function for reading zipped dgps.

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+read_zipped_dgp <- function(dir_name, id) {
+  tar <- file.path("data/dgps", dir_name, paste0(dir_name, ".zip"))
+  file <- unzip(tar, list = TRUE)$Name[id]
+  tryCatch({
+    con = gzcon(unz(tar, file))
+    readRDS(con)
+  }, finally = {
+    close(con)
+  })
+}
+
+# read_zip("DGP_3_0_1_FALSE", 1)
