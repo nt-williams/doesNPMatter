4b0b71fe0e8c3aa2811428fc1a06b2e62d963921
nt-williams <ntwilliams.personal@gmail.com>
2022-11-29 14:56:21 -0800

    Modifying for to handle all sample sizes.

--- scripts/results_cvtmle.R
+++ scripts/results_cvtmle.R
@@ -1,11 +1,10 @@
 suppressPackageStartupMessages(library(tidyverse))
-
 source("R/utils.R")
 
-dgp <- "DGP_5_1_3_TRUE"
+dgp <- "DGP_5_1_2_TRUE"
 
 DGPs <- map_dfr(1:500, function(id) {
-  meta <- try(read_dgp(dgp, id))
+  meta <- try(read_dgp(dgp, id), silent = TRUE)
   if (inherits(meta, "try-error")) return(NULL)
   data.frame(
     id = id,
@@ -18,10 +17,12 @@ DGPs <- map_dfr(1:500, function(id) {
   )
 })
 
-res <-
-  read_zip(glue::glue("data/sims/sim_cvtmle_{dgp}.zip")) |> 
-  bind_rows() |> 
-  left_join(DGPs)
+res <- map_dfr(c('100' = 100, '500' = 500, '1000' = 1000), function(n) {
+  read_zip(glue::glue("data/sims/sim_cvtmle_{dgp}_{n}.zip")) |> 
+    bind_rows() |> 
+    left_join(DGPs)
+}, .id = "n") |> 
+  mutate(n = as.numeric(n))
 
 saveRDS(res, glue::glue("data/sims/res_cvtmle_{dgp}.rds"))
 
@@ -30,7 +31,7 @@ covered <- \(l, u, truth) between(truth, l, u)
 coverage <- group_by(res, n, id) |> 
   summarise(cvtmle = mean(pmap_lgl(list(cvtmle.conf.low, cvtmle.conf.high, truth), covered)))
 
-coverage <- select(res, id, eta, rho, max_ipw) |> 
+coverage <- select(res, id, n, eta, rho, max_ipw) |> 
   distinct() |> 
   left_join(coverage)
 
